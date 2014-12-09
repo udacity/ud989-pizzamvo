904cdf34822786c466a38f6d8b09c699724be664
Ben Jaffe <ben@udacity.com>
2014-12-08 17:58:15 -0800

    init

---
+++ js/app.js
@@ -0,0 +1,76 @@
+$(function() {
+
+    var data = {
+        lastID: 0,
+        pizzas: []
+    };
+
+
+    var octopus = {
+        addPizza: function() {
+            var thisID = ++data.lastID;
+
+            data.pizzas.push({
+                id: thisID,
+                visible: true
+            });
+            view.render();
+        },
+
+        removePizza: function(pizza) {
+            var clickedPizza = data.pizzas[ pizza.id - 1 ];
+            clickedPizza.visible = false;
+            view.render();
+        },
+
+        getVisiblePizzas: function() {
+            var visiblePizzas = data.pizzas.filter(function(pizza) {
+                return pizza.visible;
+            });
+            return visiblePizzas;
+        },
+
+        init: function() {
+            view.init();
+        }
+    }
+
+
+    var view = {
+        init: function() {
+            var addPizzaBtn = $('.add-pizza');
+            addPizzaBtn.click(function() {
+                octopus.addPizza();
+            });
+
+            // grab elements and html for using in the render function
+            this.$pizzaList = $('.pizza-list');
+            this.pizzaTemplate = $('script[data-template="pizza"]').html();
+
+            // Delegated event to listen for removal clicks
+            this.$pizzaList.on('click', '.remove-pizza', function(e) {
+                var pizza = $(this).parents('.pizza').data();
+                octopus.removePizza(pizza);
+                return false;
+            });
+
+            this.render();
+        },
+
+        render: function() {
+            // Cache vars for use in forEach() callback (performance)
+            var $pizzaList = this.$pizzaList,
+                pizzaTemplate = this.pizzaTemplate;
+
+            // Clear and render
+            $pizzaList.html('');
+            octopus.getVisiblePizzas().forEach(function(pizza) {
+                // Replace template markers with data
+                var thisTemplate = pizzaTemplate.replace(/{{id}}/g, pizza.id);
+                $pizzaList.append(thisTemplate);
+            });
+        }
+    };
+
+    octopus.init();
+}());
